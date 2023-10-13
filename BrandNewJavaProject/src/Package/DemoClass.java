30c877c2f7493440912ddcd9d8a137b7ab4e1cd6
Ksenia <ksenia.qaengineer@gmail.com>
2023-10-12 20:27:49 -0400

    My first commit

---
+++ BrandNewJavaProject/src/Package/DemoClass.java
@@ -0,0 +1,27 @@
+package Package;
+
+public class DemoClass {
+	int number1 = 1;
+	float number2 = -3f;
+	String name1 = "name";
+	String name3 = "Name";
+	
+	public void mulTwonumbers (int A, int B) {
+		int C = A * B;
+		System.out.println (C);
+	
+	}
+	public void addNumbers () {
+		int a = 22;
+		int b = 55;
+		int c = a + b;
+		System.out.println (c);
+	}
+	public void printName (String name1) {
+		System.out.println (name1);
+	}
+	public void printJr () {
+		String name2 = "John Ross";
+		System.out.println (name2);
+	}
+}
---
+++ BrandNewJavaProject/src/Package/RunnerClass.java
@@ -0,0 +1,10 @@
+package Package;
+
+public class RunnerClass {
+
+	public static void main(String[] args) {
+		DemoClass d 
+		}
+	}
+
+}
